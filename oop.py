efa60d4d68ab8f8cc8aff5da789f6eeef7de3879
unknown <andrei.rossvik@gmail.com>
2021-12-15 14:41:40 +0400

    dz-15.12.21

--- oop.py
+++ oop.py
@@ -547,62 +547,111 @@
 # ============================================
 
 
-class Point:
-    WIDTH = 5
+# class Point:
+#     WIDTH = 5
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __setattr__(self, key, value):
+#         if key == "WIDTH":
+#             raise AttributeError
+#         else:
+#
+#
+#     def check_val(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def set_coords(self, x, y):
+#         if Point.check_val(x) and Point.check_val(y):
+#             self.__x = x
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coords_x(self, x):
+#         if Point.check_val(x):
+#             self.__x = x
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def set_coords_y(self, y):
+#         if Point.check_val(y):
+#             self.__y = y
+#         else:
+#             print("Координаты должны быть числами")
+#
+#     def get_coords(self):
+#         return self.__x, self.__y
+#
+#     def get_coords_x(self):
+#         return self.__x
+#
+#     def get_coords_y(self):
+#         return self.__y
+#
+#     def area(self):
+#         return (self.__x * self.__y) + Point.WIDTH
+#
+#
+#
+# p1 = Point(5, 10)
+# p1.WiDTH = 10
+# print(p1.area())
 
-    def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
 
-    def __setattr__(self, key, value):
-        if key == "WIDTH":
-            raise AttributeError
-        else:
+# DZ-15.12.21
+import math
 
 
-    def check_val(z):
-        if isinstance(z, int) or isinstance(z, float):
+class Sphere:
+
+    def __init__(self, r=0, x=0, y=0, z=0):
+        self.r = r
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def set_radius(self, r):
+        self.r = r
+
+    def get_volume(self):
+        return (self.r ** 3) * math.pi * 4 / 3
+
+    def get_square(self):
+        return (self.r ** 2) * math.pi * 4
+
+    def get_radius(self):
+        return self.r
+
+    def get_center(self):
+        return self.x, self.y, self.z
+
+    def set_center(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def is_point_inside(self, x, y, z):
+        if (self.x - x) ** 2 + (self.y - y) ** 2 + (self.z - z) ** 2 <= self.r ** 2:
             return True
         return False
 
-    def set_coords(self, x, y):
-        if Point.check_val(x) and Point.check_val(y):
-            self.__x = x
-            self.__y = y
-        else:
-            print("Координаты должны быть числами")
 
-    def set_coords_x(self, x):
-        if Point.check_val(x):
-            self.__x = x
-        else:
-            print("Координаты должны быть числами")
-
-    def set_coords_y(self, y):
-        if Point.check_val(y):
-            self.__y = y
-        else:
-            print("Координаты должны быть числами")
-
-    def get_coords(self):
-        return self.__x, self.__y
-
-    def get_coords_x(self):
-        return self.__x
-
-    def get_coords_y(self):
-        return self.__y
-
-    def area(self):
-        return (self.__x * self.__y) + Point.WIDTH
-
-
-
-p1 = Point(5, 10)
-p1.WiDTH = 10
-print(p1.area())
-
-
+sp1 = Sphere()
+# sp1.r = 0.6
+sp1.set_radius(0.6)
+print("get_radius: ", sp1.get_radius())
+print("get_volume: ", sp1.get_volume())
+print("get_square: ", sp1.get_square())
+print("get_center: ", sp1.get_center())
+print("is_point_inside:(0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
+sp1.set_radius(1.6)
+print("get_radius: ", sp1.get_radius())
+print("is_point_inside: (0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
 
 
 
