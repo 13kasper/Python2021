81894ebc1dff50f0b7ee67aae096d5a8c0c4595c
kas <andrei.rossvik@gmail.com>
2021-12-16 22:21:57 +0400

    statik metod

--- oop.py
+++ oop.py
@@ -604,54 +604,348 @@
 
 
 # DZ-15.12.21
-import math
+# import math
+#
+#
+# class Sphere:
+#
+#     def __init__(self, r=0, x=0, y=0, z=0):
+#         self.r = r
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#     def set_radius(self, r):
+#         self.r = r
+#
+#     def get_volume(self):
+#         return (self.r ** 3) * math.pi * 4 / 3
+#
+#     def get_square(self):
+#         return (self.r ** 2) * math.pi * 4
+#
+#     def get_radius(self):
+#         return self.r
+#
+#     def get_center(self):
+#         return self.x, self.y, self.z
+#
+#     def set_center(self, x, y, z):
+#         self.x = x
+#         self.y = y
+#         self.z = z
+#
+#     def is_point_inside(self, x, y, z):
+#         if (self.x - x) ** 2 + (self.y - y) ** 2 + (self.z - z) ** 2 <= self.r ** 2:
+#             return True
+#         return False
+#
+#
+# sp1 = Sphere()
+# # sp1.r = 0.6
+# sp1.set_radius(0.6)
+# print("get_radius: ", sp1.get_radius())
+# print("get_volume: ", sp1.get_volume())
+# print("get_square: ", sp1.get_square())
+# print("get_center: ", sp1.get_center())
+# print("is_point_inside:(0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
+# sp1.set_radius(1.6)
+# print("get_radius: ", sp1.get_radius())
+# print("is_point_inside: (0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
+#
+#
+#
+
+# ================================================================ 16.12.21 ===============================
+# class Point:
+#     __slots__ = ["__x", "__y", "z"]  # закрываем доступ на добавление переменных кроме тех что прописанны
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#
+# p1 = Point(5, 10)
+# p1.z = 1
+# print(p1.z)
+# print(p1.__dict__)
+#
+#
+#
+# class Point:
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __cheeck_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     def __set_coords_x(self, x):  # __ закрываем доступ к методу
+#         if Point.__cheeck_value(x):
+#             self.__x = x
+#         else:
+#             raise ValueError("Неверный формат данных")
+#
+#     def __get_coords_x(self):
+#         print("Вызов __get_coords_x")
+#         return self.__x
+#
+#     def __del_coords_x(self):
+#         print("Удаление свойства")
+#         del self.__x
+#
+#     coordX = property(__get_coords_x, __set_coords_x, __del_coords_x)  # один из способов
+#
+#
+# p1 = Point(5, 10)
+# p1.coordX = 100
+# print(p1.coordX)
+# del p1.coordX
+# p1.coordX = 7
+# print(p1.coordX)
+
+#
+#
+# class Point:
+#
+#     def __init__(self, x, y):
+#         self.__x = x
+#         self.__y = y
+#
+#     def __cheeck_value(z):
+#         if isinstance(z, int) or isinstance(z, float):
+#             return True
+#         return False
+#
+#     @property  # декоратор ======================================
+#     def coords_x(self):  # первым должен быть GETer
+#         print("Вызов __get_coords_x")
+#         return self.__x
+#
+#     @coords_x.setter  # прописываем название метода + setter
+#     def coords_x(self, x):  # __ закрываем доступ к методу
+#         if Point.__cheeck_value(x):
+#             self.__x = x
+#         else:
+#             raise ValueError("Неверный формат данных")
+#
+#     @coords_x.deleter
+#     def coords_x(self):
+#         print("Удаление свойства")
+#         del self.__x
+#
+#     # coordX = property(__get_coords_x, __set_coords_x, __del_coords_x)  # один из способов
+#
+#
+# p1 = Point(5, 10)
+# p1.coords_x = 100
+# print(p1.coords_x)
+# # del p1.coords_x
+# p1.coords_x = 7
+# print(p1.coords_x)
+# print(p1.__dict__)
+
+# ================================================== ЗАДАЧА перевести киллограмы в пуды
+
+# class KgToPounds:
+#     def __init__(self, kg):
+#         self.__kg = kg
+#
+#     @property
+#     def kg(self):
+#         return  self.__kg
+#
+#     @kg.setter
+#     def kg(self, new_kg):
+#         if isinstance(new_kg, (int, float)):
+#             self.__kg = new_kg
+#         else:
+#             print("Киллограмы задаются только числами")
+#
+#     def to_pounds(self):
+#         return self.__kg * 2.205
+#
+#
+# w = KgToPounds(12)
+# print(w.to_pounds())
+# w.kg = 41
+# print(w.kg)
+# print(w.to_pounds())
+# w.kg = "fff"
+# print(w.to_pounds())
+
+# ================================================== СТАТИЧЕСКИЙ МЕТОД
+
+# class Point:
+#     __count = 0  # закрытая статическая переменная
+#
+#     def __init__(self, x=0, y=0):
+#         self.__x = x
+#         self.__y = y
+#         Point.__count += 1
+#
+#     @staticmethod  # статический метод ( можно вызывать и через имя класса и через имя экземпляра )
+#     def get_count():
+#         return Point.__count
+#
+#
+# p1 = Point()
+# p2 = Point()
+# p3 = Point()
+#
+# print(Point.get_count())
+# print(p1.get_count())
+
+#
+#
+# class Change:
+#     @staticmethod
+#     def inc(x):
+#         return x + 1
+#
+#     @staticmethod
+#     def dec(x):
+#         return x - 1
+#
+#
+# print(Change.inc(10), Change.dec(10))
+# q = Change()
+# print(q.dec(5), q.inc(5))
 
 
-class Sphere:
+# ===================================================== ЗАДАЧА =================================
 
-    def __init__(self, r=0, x=0, y=0, z=0):
-        self.r = r
-        self.x = x
-        self.y = y
-        self.z = z
+# class Numbers:
+#
+#     @staticmethod
+#     def max(a, b, c, d):
+#         if a > b and a > c and a > d:
+#             return a
+#         if b > a and b > c and b > d:
+#             return b
+#         if c > a and c > b and c > d:
+#             return c
+#         if d > a and d > b and d > c:
+#             return d
+#
+#     @staticmethod
+#     def min(a, b, c, d):
+#         if a < b and a < c and a < d:
+#             return a
+#         if b < a and b < c and b < d:
+#             return b
+#         if c < a and c < b and c < d:
+#             return c
+#         if d < a and d < b and d < c:
+#             return d
+#
+#     @staticmethod
+#     def mid(a, b, c, d):
+#         return (a + b + c + d)/4
+#
+#     @staticmethod
+#     def fact(x):
+#         fac = 1
+#         for i in range(1, x + 1):  # факториал числа
+#             fac *= i
+#         return fac
+#
+#
+# print(Numbers.max(1, 2, 3, 4))
+# print(Numbers.min(1, 2, 3, 4))
+# print(Numbers.mid(1, 2, 3, 4))
+# print(Numbers.fact(4))
 
-    def set_radius(self, r):
-        self.r = r
+# =============================================
 
-    def get_volume(self):
-        return (self.r ** 3) * math.pi * 4 / 3
+# ================================================= МЕТОДЫ КЛАССА ============================= ПЕРЕСМОТРЕТЬ !!!!
 
-    def get_square(self):
-        return (self.r ** 2) * math.pi * 4
+# class Date:
+#     def __init__(self, day=0, mont=0, year=0):
+#         self.day = day
+#         self.mont = mont
+#         self.year = year
+#
+#     @classmethod
+#     def from_string(cls, date_as_string):  # cls принимает класс ( self принмает экземпляр )
+#         day, mont, year = map(int, date_as_string.split("."))
+#         date1 = cls(day, mont, year)
+#         return date1
+#
+#     @staticmethod
+#     def is_date_valid(date_as_string):
+#         if date_as_string.count('.') == 2:
+#             day, mont, year = map(int, date_as_string.split("."))
+#             return day <= 31 and mont <= 12 and year <= 3999
+#
+#     def string_to_db(self):
+#         return f'{self.year}-{self.mont}-{self.day}'
+#
+#
+# dates = [
+#     '30.12.2021'
+#     '30-12-2020'
+#     '01.01.2021'
+#     '12.31.2020'
+# ]
+#
+# for d in dates:
+#     if Date.is_date_valid(d):
+#         date = Date.from_string(d)
+#         db = date.string_to_db()
+#         print(db)
+#     else:
+#         print("Неправильная дата или формат строки с датой")
+#
+#
+#
+# # d = "16.12.2021"
+# # day, mont, year = map(int, d.split("."))
+# # print(day, mont, year)
+# # d = Date()
+# # date = Date.from_string("16.12..2021")
+# # print(date.string_to_db())
+# # d1 = Date()
+# # date1 = d1.from_string("25.12.2021")
+# # print(date1.string_to_db())
+# #
+# # date2 = Date.from_string("15.10.2110")
+# # print(date2.string_to_db())
 
-    def get_radius(self):
-        return self.r
 
-    def get_center(self):
-        return self.x, self.y, self.z
+# ========================================================================= !!!!!
 
-    def set_center(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def is_point_inside(self, x, y, z):
-        if (self.x - x) ** 2 + (self.y - y) ** 2 + (self.z - z) ** 2 <= self.r ** 2:
-            return True
-        return False
-
-
-sp1 = Sphere()
-# sp1.r = 0.6
-sp1.set_radius(0.6)
-print("get_radius: ", sp1.get_radius())
-print("get_volume: ", sp1.get_volume())
-print("get_square: ", sp1.get_square())
-print("get_center: ", sp1.get_center())
-print("is_point_inside:(0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
-sp1.set_radius(1.6)
-print("get_radius: ", sp1.get_radius())
-print("is_point_inside: (0, 1.5, 0):", sp1.is_point_inside(0, -1.5, 0))
-
-
-
+# # ====================== ЗАДАЧА
+#
+# class Ploshad:
+#     count = 0
+#
+#     @staticmethod
+#     def rectangle_geron(a, b, c):
+#         perimetr = (a + b + c) / 2
+#         return (perimetr * (perimetr - a) * (perimetr - b) * (perimetr - c)) ** 0.5
+#
+#     @staticmethod
+#     def osnovanie_visota(a, h):
+#         return 0.5 * h * a
+#
+#     @staticmethod
+#     def ploshad_sq(a):
+#         return a ** 2
+#
+#     @staticmethod
+#     def ploshad_pryam(a, b):
+#         Ploshad.count += 1
+#         return a * b
+#
+#
+# print(Ploshad.rectangle_geron(3, 4, 5))
+# print(Ploshad.osnovanie_visota(6, 7))
+# print(Ploshad.ploshad_sq(7))
+# print(Ploshad.ploshad_pryam(2, 6))
+# try_t = Ploshad
+# print(try_t.rectangle_geron(1, 2, 3))
+# print(Ploshad.count)
